a53e92efbdb9ac9a7318d96238fbde264e578471
Anis Ghliss <anisghliss20@gmail.com>
2017-11-20 21:44:52 +0000

    Done.

--- lib/play.rb
+++ lib/play.rb
@@ -15,6 +15,8 @@ def move(board, index, current_player = "X")
   board[index] = current_player
 end
 
+
+
 def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
@@ -35,4 +37,10 @@ def turn(board)
   end
 end
 
-# Define your play method below
+def play(board)
+counter=0
+  while(counter < 9)
+    turn(board)
+    counter+=1
+  end
+end
